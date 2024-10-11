1717ec92577febe6e9ddc4ea0f194443c72a6a18
Nicolas Osborne <nho.osborne@gmail.com>
2024-10-11 13:00:44 +0200

    Define routes with React-Router in App.jsx - Create Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path='/product/:id' element{<Product />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/confirmation' element={<Confirmation />} />
+        <Route path='*' element={<Error />} /> */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { Link, NavLink } from "react-router-dom";
+import { FaRegUser, FaShoppingCart } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/" className="header-logo">
+        <img
+          src="./src/assets/logos/logo-header.svg"
+          alt="Logo Glou & Glou"
+          height={70}
+          width={260}
+        />
+      </Link>
+      <nav className="header-nav">
+        <NavLink to="/login" className="header-nav_login">
+          <FaRegUser />
+          Connexion
+        </NavLink>
+        <NavLink to="/cart" className="header-nav_cart">
+          <FaShoppingCart />
+          Panier (0)
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
