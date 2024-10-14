83ef8d3fb73bd1fa1b7ee57d0377b792841857d6
Nicolas Osborne <nho.osborne@gmail.com>
2024-10-14 10:17:08 +0200

    Fix responsive display on Product Page

--- src/components/QuantitySelector.jsx
+++ src/components/QuantitySelector.jsx
@@ -1,29 +1,29 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 const QuantitySelector = () => {
-  const [productQuantity, setProductQuantity] = useState(1);
+  const [productQuantity, setProductQuantity] = useState(1)
 
   const handleDecrement = () => {
     if (productQuantity > 1) {
-      setProductQuantity(productQuantity - 1);
+      setProductQuantity(productQuantity - 1)
     }
-  };
+  }
 
   const handleIncrement = () => {
-    setProductQuantity(productQuantity + 1);
-  };
+    setProductQuantity(productQuantity + 1)
+  }
 
   return (
-    <div className="quantity-selector">
-      <button onClick={handleDecrement} className="quantity-selector_decrement">
+    <div className='quantity-selector'>
+      <button onClick={handleDecrement} className='quantity-selector_decrement'>
         -
       </button>
-      <span className="quantity-selector_quantity">{productQuantity}</span>
-      <button onClick={handleIncrement} className="quantity-selector_increment">
+      <span className='quantity-selector_quantity'>{productQuantity}</span>
+      <button onClick={handleIncrement} className='quantity-selector_increment'>
         +
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default QuantitySelector;
+export default QuantitySelector
