b855ce8a37ca497137a7658bd8f39ff6b918cd03
Nicolas Osborne <nho.osborne@gmail.com>
2024-10-15 09:12:40 +0200

    Add mocked data with JSON files - Dynamic rendering of Products

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,60 +1,73 @@
-import ProductCard from "./ProductCard";
-import BeerImage from "../assets/products/beer.svg";
-import WineImage from "../assets/products/wine.svg";
-import AlcoholImage from "../assets/products/alcohol.svg";
-import SoftDrinkImage from "../assets/products/soft-drink.svg";
-import HotDrinkImage from "../assets/products/hot-drink.svg";
+import ProductCard from './ProductCard'
+import BeerImage from '../assets/products/beer.svg'
+import WineImage from '../assets/products/wine.svg'
+import AlcoholImage from '../assets/products/alcohol.svg'
+import SoftDrinkImage from '../assets/products/soft-drink.svg'
+import HotDrinkImage from '../assets/products/hot-drink.svg'
+
+import products from '../mockDatabase/products.json'
+import categories from '../mockDatabase/categories.json'
+import { Link } from 'react-router-dom'
 
 const ProductList = () => {
+  const getProductImageSrc = (category) => {
+    switch (category) {
+      case 1:
+        return BeerImage
+      case 2:
+        return WineImage
+      case 3:
+        return AlcoholImage
+      case 4:
+        return SoftDrinkImage
+      case 5:
+        return HotDrinkImage
+      default:
+        return null
+    }
+  }
+
+  const getProductImageAlt = (category) => {
+    switch (category) {
+      case 1:
+        return 'Bière'
+      case 2:
+        return 'Vin'
+      case 3:
+        return 'Spiritueux'
+      case 4:
+        return 'Sans Alcool'
+      case 5:
+        return 'Boisson Chaude'
+      default:
+        return null
+    }
+  }
+
+  const getProductCategory = (category) => {
+    const categoryData = categories.find((c) => c.id === category)
+    return categoryData ? categoryData.name : null
+  }
+
   return (
-    <div className="products">
-      <h1 className="products-title">Nouveautés</h1>
-      <div className="products-container">
-        <ProductCard
-          productImageSrc={BeerImage}
-          productImageAlt="Bière"
-          productName="West Coast IPA"
-          productCategory="Bières - IPA"
-          productPrice={6.3}
-        />
-        <ProductCard
-          productImageSrc={BeerImage}
-          productImageAlt="Bière"
-          productName="Czech Pilsner"
-          productCategory="Bières - Blonde"
-          productPrice={4.2}
-        />
-        <ProductCard
-          productImageSrc={WineImage}
-          productImageAlt="Vin"
-          productName="Syrah"
-          productCategory="Vins - rouge"
-          productPrice={16.4}
-        />
-        <ProductCard
-          productImageSrc={SoftDrinkImage}
-          productImageAlt="Sans alcool"
-          productName="Club Mate"
-          productCategory="Sans Alcool - Soda"
-          productPrice={3.8}
-        />
-        <ProductCard
-          productImageSrc={AlcoholImage}
-          productImageAlt="Spiritueux"
-          productName="Mezcal"
-          productCategory="Spiritueux - Tequila"
-          productPrice={18.6}
-        />
-        <ProductCard
-          productImageSrc={HotDrinkImage}
-          productImageAlt="Boisson chaude"
-          productName="Blue Jamaica"
-          productCategory="Boissons Chaudes - Café"
-          productPrice={9.2}
-        />
+    <div className='products'>
+      <h1 className='products-title'>Nouveautés</h1>
+      <div className='products-container'>
+        {products.map((product) => (
+          <Link to={`/product/${product.id}`} key={product.id}>
+            <ProductCard
+              key={product.id}
+              productImageSrc={getProductImageSrc(product.category)}
+              productImageAlt={getProductImageAlt(product.category)}
+              productName={product.name}
+              productCategory={getProductCategory(product.category)}
+              productPrice={product.price}
+            />
+          </Link>
+        ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductList;
+export default ProductList
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,33 +2,104 @@ import Filters from '../components/Filters'
 import QuantitySelector from '../components/QuantitySelector'
 import Button from '../components/Button'
 
+import { useNavigate, useParams } from 'react-router-dom'
+
 import BeerImage from '../assets/products/beer.svg'
 import WineImage from '../assets/products/wine.svg'
 import AlcoholImage from '../assets/products/alcohol.svg'
 import SoftDrinkImage from '../assets/products/soft-drink.svg'
 import HotDrinkImage from '../assets/products/hot-drink.svg'
 
+import products from '../mockDatabase/products.json'
+
 const ProductPage = () => {
+  const navigate = useNavigate()
+
+  const { id } = useParams()
+  const product = products.find((product) => product.id === parseInt(id))
+
+  if (!product) {
+    navigate('/error')
+  }
+
+  const getProductImageSrc = (category) => {
+    switch (category) {
+      case 1:
+        return BeerImage
+      case 2:
+        return WineImage
+      case 3:
+        return AlcoholImage
+      case 4:
+        return SoftDrinkImage
+      case 5:
+        return HotDrinkImage
+      default:
+        return null
+    }
+  }
+
+  const getProductImageAlt = (category) => {
+    switch (category) {
+      case 1:
+        return 'Bière'
+      case 2:
+        return 'Vin'
+      case 3:
+        return 'Spiritueux'
+      case 4:
+        return 'Sans Alcool'
+      case 5:
+        return 'Boisson Chaude'
+      default:
+        return null
+    }
+  }
+
+  const getProductCategory = (category) => {
+    switch (category) {
+      case 1:
+        return 'Bières'
+      case 2:
+        return 'Vins'
+      case 3:
+        return 'Spiritueux'
+      case 4:
+        return 'Sans Alcool'
+      case 5:
+        return 'Boissons Chaudes'
+      default:
+        return null
+    }
+  }
+
   return (
     <section className='product-page'>
       <div className='product-page_content-container'>
         <Filters />
         <div className='product-container'>
           <div className='product_image'>
-            <img src={BeerImage} alt='Bière' height={150} width={150} />
+            <img
+              src={getProductImageSrc(product.category)}
+              alt={getProductImageAlt(product.category)}
+              height={150}
+              width={150}
+            />
           </div>
           <div className='product-details'>
-            <h2 className='product-details_name'>West Coast IPA</h2>
-            <h3 className='product-details_category'>Bières - IPA</h3>
-            <p className='product-details_description'>
-              Bière aggressivement houblonnée au Nelson Sauvin, Motueka et
-              Riwaka. <br />
-              7.2% <br />
-              44cL
+            <h2 className='product-details_name'>{product.name}</h2>
+            <h3 className='product-details_category'>
+              {getProductCategory(product.category)}
+            </h3>
+            <p className='product-details_description'>{product.description}</p>
+            <p className='product-details_price'>{product.price} €</p>
+            <p
+              className={`product-details_status ${
+                product.quantity > 0 ? 'in-stock' : 'out-of-stock'
+              }`}
+            >
+              {product.quantity > 0 ? 'En stock' : 'Rupture de stock'}
             </p>
-            <p className='product-details_price'>6,30 €</p>
-            <p className='product-details_in-stock'>En stock</p>
-            <p className='product-details_out-of-stock'>Rupture de stock</p>
             <div className='product-details_add-to-cart-container'>
               <QuantitySelector />
               <Button
